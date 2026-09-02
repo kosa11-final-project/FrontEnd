03f68edd841cc5e6e17bae9be1898a4f7d9a0e35
WOOJIN CHOI <dnwls0723@sookmyung.ac.kr>
2026-09-02 21:57:15 +0900

    Revert "feat: 대시보드 및 AI 전략 시뮬레이션 UX 개선 (#131)"
    This reverts commit 59eb8f6a8b2556667787e08808d7b841f17a399c, reversing
    changes made to 83729a24ef3afa5cc3e126a2f3673e8175d386a2.

--- src/entities/strategy/index.js
+++ src/entities/strategy/index.js
@@ -25,7 +25,6 @@ export {
   buildStrategyChartData,
   getStrategyAdjustmentValidationError,
   getStrategyAdjustmentDefaults,
-  getStrategyEndDateMaximum,
   getSimulationComparisonRows,
   resolveStrategyActionType,
   resolveStrategyLocationPresentation,
